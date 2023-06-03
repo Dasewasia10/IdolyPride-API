c83c045f62c90c394ff17e7567936def729e0576
Dasewasia10 <daniel.stonford10@gmail.com>
2023-06-03 22:42:18 +0800

    minor change App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import MainPage from "../page/mainPage";
@@ -8,9 +8,7 @@ function App() {
 	return (
 		<Router>
 			<div>
-				<Routes>
-					<Route path="/" element={<MainPage />} />
-				</Routes>
+				<Route path="/" component={MainPage} />
 			</div>
 		</Router>
 	);
