0825318601fd422418ddf1ab364b2cb9fdeff195
Dasewasia10 <daniel.stonford10@gmail.com>
2023-06-03 14:52:23 +0800

    add vercel.json

--- main.py
+++ main.py
@@ -120,6 +120,5 @@ def delete_idol(id):
 def home():
     return jsonify({'message': 'API is running'})
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
