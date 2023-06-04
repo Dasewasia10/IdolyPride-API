17bd3bb2b5afe319ecba958ca8efa12d5c8c25e4
Dasewasia10 <daniel.stonford10@gmail.com>
2023-06-04 08:39:54 +0800

    add dark mode

--- page/mainPage.jsx
+++ page/mainPage.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const MainPage = () => {
+	const [isDarkMode, setIsDarkMode] = useState(
+		localStorage.getItem("isDarkMode") === "true"
+	);
+
 	const customBackgroundStyle = {
 		backgroundImage: "url('/assets/bg.png')",
 		backgroundSize: "cover",
@@ -9,9 +13,25 @@ const MainPage = () => {
 		height: "100vh",
 	};
 
+	useEffect(() => {
+		if (isDarkMode) {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+		}
+		localStorage.setItem("isDarkMode", isDarkMode);
+	}, [isDarkMode]);
+
+	const toggleDarkMode = () => {
+		setIsDarkMode(!isDarkMode);
+	};
 	return (
 		<>
-			<div style={customBackgroundStyle} className="bg-gray-100 h-screen">
+			<div
+				style={customBackgroundStyle}
+				className={`bg-gray-100 ${
+					isDarkMode ? "dark:bg-gray-900" : ""
+				} h-screen`}>
 				<div className="flex absolute h-8 md:h-12 lg:h-16 w-auto top-6 left-10 flex-row gap-10 items-center z-50">
 					<img
 						className="h-4 md:h-8 lg:h-12 w-auto"
@@ -26,12 +46,12 @@ const MainPage = () => {
 				</div>
 				<div className="relative z-20">
 					<img
-						className="opacity-0 lg:opacity-100 lg:h-screen w-auto absolute top-0 left-0 ml-4"
+						className="opacity-0 lg:opacity-100 h-full lg:h-screen w-auto absolute top-0 left-0 ml-4"
 						src={`/assets/1.png`}
 						alt="Hayasaka-Mei-1"
 					/>
 					<img
-						className="opacity-0 lg:opacity-100 lg:h-screen w-auto absolute top-0 right-0 mr-4"
+						className="opacity-0 lg:opacity-100 h-full lg:h-screen w-auto absolute top-0 right-0 mr-4"
 						src={`/assets/2.png`}
 						alt="Hayasaka-Mei-6"
 					/>
@@ -73,10 +93,35 @@ const MainPage = () => {
 						</div>
 					</div>
 					<div className="flex absolute text-sm bottom-8">
-						Copyright by QualiArts. Created by Dasewasia @2023
+						&copy; Copyright by QualiArts. Created by Dasewasia @2023
 					</div>
 				</div>
 			</div>
+
+			<button
+				className={`absolute z-[100] bottom-auto top-4 right-4 lg:top-auto lg:bottom-8 lg:right-8 p-2 rounded-full shadow-md ${
+					isDarkMode ? "bg-gray-200" : "bg-gray-800"
+				} transition-colors duration-300`}
+				onClick={toggleDarkMode}
+				title="Dark mode toggle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					className={`w-6 h-6 ${
+						isDarkMode ? "text-gray-800" : "text-gray-200"
+					} transition-colors duration-300`}>
+					{isDarkMode ? (
+						<path d="M21 12.79A9 9 0 0 1 12.07 21a9 9 0 0 1-9-9.94 8.998 8.998 0 0 1 4.1-7.54A4.996 4.996 0 0 0 11 5a5 5 0 0 0 0-10 1 1 0 0 1 0-2 7 7 0 1 1 6.33 10.29" />
+					) : (
+						<path d="M12 22s-6.5-2-6.5-10a6.5 6.5 0 1 1 13 0c0 8-6.5 10-6.5 10zM12 11V2" />
+					)}
+				</svg>
+			</button>
 		</>
 	);
 };
