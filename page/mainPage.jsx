325867af1eeb1959e77784e7c57dfd6e57af17c4
Dasewasia10 <daniel.stonford10@gmail.com>
2023-06-04 08:07:40 +0800

    update url in mainPage.jsx

--- page/mainPage.jsx
+++ page/mainPage.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 
 const MainPage = () => {
-	const location = useLocation();
-
 	const customBackgroundStyle = {
-		backgroundImage: `url(${location.pathname}/assets/bg.png')`,
+		backgroundImage: `url(/assets/bg.png')`,
 		backgroundSize: "cover",
 		backgroundPosition: "center",
 		backgroundRepeat: "no-repeat",
@@ -18,24 +15,24 @@ const MainPage = () => {
 				<div className="flex absolute h-8 md:h-12 lg:h-16 w-auto top-6 left-10 flex-row gap-10 items-center z-50">
 					<img
 						className="h-4 md:h-8 lg:h-12 w-auto"
-						src={`${location.pathname}/assets/pojok.png`}
+						src={`/assets/pojok.png`}
 						alt="idoly-pride-blue-logo"
 					/>
 					<img
 						className="h-8 md:h-12 lg:h-16 w-auto"
-						src={`${location.pathname}/assets/dase.png`}
+						src={`/assets/dase.png`}
 						alt="dasesplace-logo"
 					/>
 				</div>
 				<div className="relative z-20">
 					<img
 						className="opacity-0 lg:opacity-100 lg:h-screen w-auto absolute top-0 left-0 ml-4"
-						src={`${location.pathname}/assets/1.png`}
+						src={`/assets/1.png`}
 						alt="Hayasaka-Mei-1"
 					/>
 					<img
 						className="opacity-0 lg:opacity-100 lg:h-screen w-auto absolute top-0 right-0 mr-4"
-						src={`${location.pathname}/assets/2.png`}
+						src={`/assets/2.png`}
 						alt="Hayasaka-Mei-6"
 					/>
 				</div>
@@ -43,7 +40,7 @@ const MainPage = () => {
 				<div className="flex flex-col h-screen justify-center items-center p-4 mx-auto">
 					<div className="text-center">
 						<img
-							src={`${location.pathname}/assets/middle.png`}
+							src={`/assets/middle.png`}
 							alt="Gambar"
 							className="mx-auto w-24 md:w-32 lg:w-64 h-auto mb-4"
 						/>
